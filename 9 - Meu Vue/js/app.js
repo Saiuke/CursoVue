3d58a35592cc6c6d85f521759683aff2ea3c3fe3
Ezequias Lopes <ezequias@hotmail.com.br>
2020-10-16 22:00:32 -0500

    Created a mockup framework

--- 9 - Meu Vue/js/app.js
+++ 9 - Meu Vue/js/app.js
@@ -1,15 +1,33 @@
-new Vue({
+function MeuVue(param) {
+    this.$el = document.querySelector(param.el)
+    this.$data = param.data
+
+    //Setter and Getter for object properties
+    for (let atr in this.$data) {
+        Object.defineProperty(this, atr, {
+            get: () => {
+                return this.$data[atr]
+            },
+            set: value => {
+                this.$data[atr] = value
+            }
+        })
+    }
+
+    //Data interpolation
+    const regex = /\{\{([\s\w+]*)\}\}/g
+    this.$el.innerHTML = this.$el.innerHTML.replace(regex, (match, $1) => {
+        const value = this[$1.trim()]
+        return value ? value : eval($1)
+    })
+}
+
+const vm = new MeuVue({
     el: '#app',
     data: {
-        
-    },
-    computed: {
-
-    },
-    watch: {
-
-    },
-    methods: {
-
+        nome: 'Maria',
+        sobrenome: 'das Dores',
     }
-});
+})
+
+console.log(vm.nome);
