1b471f405097b53ebc5937869335c20a9e222ba7
Ezequias Lopes <ezequias@hotmail.com.br>
2020-12-26 11:45:23 -0500

    Different way mount the Vue Instance

--- 12 - Montando Template/js/app.js
+++ 12 - Montando Template/js/app.js
@@ -1,4 +1,12 @@
 const vm = new Vue({
+    template: `
+    <div>
+        <h1>{{ aula }}</h1>
+        <h2>{{ modulo }}</h2>
+        <button @click="alterarAula">Alterar aula</button>
+        <button @click="alterarModulo">Alterar módulo</button>
+    </div>
+    `,
     data: {
         aula: 'Usando Refs',
         modulo: 'Módulo: Instancia Vue'
