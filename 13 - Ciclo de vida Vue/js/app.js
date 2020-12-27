98a369f792da20f3bab24d7075eccbef41c09c12
Ezequias Lopes <ezequias@hotmail.com.br>
2020-12-27 11:18:02 -0500

    New Vue Project

--- 13 - Ciclo de vida Vue/js/app.js
+++ 13 - Ciclo de vida Vue/js/app.js
@@ -3,6 +3,30 @@ new Vue({
     data: {
         titulo: 'Ciclo de vida',
     },
+    beforeCreate(){
+        console.log('Antes de criar')
+    },
+    created(){
+        console.log('Criado')
+    },
+    beforeMount(){
+        console.log('Antes de montar')
+    },
+    mounted(){
+        console.log('Montado')
+    },
+    beforeUpdate(){
+        console.log('Antes de atulizado')
+    },
+    updated(){
+        console.log('Atualizado')
+    },
+    beforeDestroy(){
+        console.log('Antes de destruir')
+    },
+    destroyed(){
+        console.log('Bye')
+    },
     computed: {
 
     },
