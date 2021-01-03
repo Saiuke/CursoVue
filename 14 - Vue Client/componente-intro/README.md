f35ce9a5e6c1967936f7d09e55b4b111ece91c69
Ezequias Lopes <ezequias@hotmail.com.br>
2021-01-03 15:46:16 -0500

    Deleted some test projects

--- 14 - Vue Client/componente-intro/README.md
+++ 14 - Vue Client/componente-intro/README.md
@@ -1,4 +1,4 @@
-# projetoexemplo
+# componente-intro
 
 ## Project setup
 ```
--- 14 - Vue Client/componente-intro/src/main.js
+++ 14 - Vue Client/componente-intro/src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Contador from './components/Contador.vue'
 
 Vue.config.productionTip = false
-Vue.component('app-contador', Contador)
 
 new Vue({
   render: h => h(App),
