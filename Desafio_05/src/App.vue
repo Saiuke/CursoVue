061f5310f91d93e7a7d6936548df2e2211f4d6c7
Ezequias Lopes <ezequias@hotmail.com.br>
2021-01-03 18:19:53 -0500

    Created Desafio_5

---
+++ Desafio_05/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+          <app-menu/>
+    </v-navigation-drawer>
+    <v-app-bar app>
+        <app-toolbar/>
+    </v-app-bar>
+
+    <v-content>
+      <app-carousel />
+    </v-content>
+    <app-footer/>
+  </v-app>
+</template>
+
+<script>
+
+import Footer from './Footer.vue'
+import Carousel from './Carousel.vue'
+import Menu from './Menu.vue'
+import Toolbar from './Toolbar.vue'
+
+export default {
+  components: {
+    'app-footer': Footer,
+    'app-carousel': Carousel,
+    'app-menu': Menu,
+    'app-toolbar': Toolbar,
+
+  }
+}
+</script>
